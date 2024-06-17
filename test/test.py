790ffad78cedd1775deb70a693787b45562a1fbf
Vladislav <vladrosenrol@gmail.com>
2024-06-17 23:38:46 +0300

    creat test for post

--- test/test.py
+++ test/test.py
@@ -1,10 +1,18 @@
-from fastapi import FastAPI
-from fastapi.testclient import TestClient
 
-app = FastAPI()
+from fastapi.testclient import TestClient
+from main import app
+
 client = TestClient(app)
+
 
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
 
+def test_create_users():
+    response = client.post(
+        "/create_user",
+        json={},
+    )
+    assert response.status_code == 200
+    assert response.json() == {}
