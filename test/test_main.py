0fcd98bf00c778922b465612efb729244c1a0a5b
Vladislav <vladrosenrol@gmail.com>
2024-06-18 00:13:18 +0300

    add in test

---
+++ test/test_main.py
@@ -0,0 +1,30 @@
+from fastapi.testclient import TestClient
+from fastapi import FastAPI
+
+app = FastAPI()
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/users")
+    assert response.status_code == 200
+
+def test_create_users():
+    response = client.post(
+        "/create_user",
+        json={
+            "name": "test_pytest",
+            "last_name": "test_pytest",
+            "login": "test_pytest",
+            "password": "test_pytest",
+            "email": "test_pytest@gmail.com"
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+            "name": "test_pytest",
+            "last_name": "test_pytest",
+            "login": "test_pytest",
+            "password": "test_pytest",
+            "email": "test_pytest@gmail.com"
+        }
