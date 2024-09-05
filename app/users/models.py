2ab98c0a99882116d6ea45f782a87de7648b186a
Vladislav <vladrosenrol@gmail.com>
2024-09-05 22:00:53 +0300

    Добавил отпуск и командировки

--- app/users/models.py
+++ app/users/models.py
@@ -9,7 +9,12 @@ class User(Model):
     login = fields.CharField(max_length=250)
     password = fields.CharField(max_length=250)
     email = fields.CharField(max_length=250)
+    start_vacation = fields.DateField(null=True)
+    finish_vacation = fields.DateField(null=True)
+    start_business_trip = fields.DateField(null=True)
+    finish_business_trip = fields.DateField(null=True)
     division = fields.ForeignKeyField('models.Division', on_delete=fields.CASCADE, null=True)
 
     def __str__(self):
         return self.name
+
